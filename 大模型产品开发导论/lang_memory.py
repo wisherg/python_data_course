8f7bb3ef3cb361d54612c13492fd6217dff4002d
wisherg <wangpenge@gmail.com>
2024-05-07 17:25:30 +0800

    Update lang_memory.py

--- 大模型产品开发导论/lang_memory.py
+++ 大模型产品开发导论/lang_memory.py
@@ -1,43 +1,13 @@
 
 import streamlit as st
-import numpy as np
-
 import os
 from langchain_community.llms import Tongyi
-from langchain_community.llms import SparkLLM
-from langchain_community.llms import QianfanLLMEndpoint
-
-import pandas as pd
 #导入模版
 from langchain.prompts import PromptTemplate
-
-#导入聊天模型
-from langchain.prompts.chat import (
-    ChatPromptTemplate,
-    SystemMessagePromptTemplate,
-    AIMessagePromptTemplate,
-    HumanMessagePromptTemplate,
-)
-from langchain.schema import (
-    AIMessage,
-    HumanMessage,
-    SystemMessage
-)
-
-from langchain_community.chat_models import ChatSparkLLM
-from langchain_community.chat_models.tongyi import ChatTongyi
-from langchain_community.chat_models import QianfanChatEndpoint
-
 #输入三个模型各自的key
 
-os.environ["DASHSCOPE_API_KEY"] = "sk-39a2d85e79a5493a8859b60b725e5e55"
+os.environ["DASHSCOPE_API_KEY"] = ""
 
-os.environ["IFLYTEK_SPARK_APP_ID"] = "8a0a858a"
-os.environ["IFLYTEK_SPARK_API_KEY"] = "074dfcac2a41dae77ae98d8400dcca85"
-os.environ["IFLYTEK_SPARK_API_SECRET"] = "YTk0M2Q1YzNmMTNjY2JkNGI1MWY5MDg2"
-
-os.environ["QIANFAN_AK"] = "og6mWrRcqJqb1a8aiDl81tyC"
-os.environ["QIANFAN_SK"] = "QV7NVmwMlXwoA67TOWu3lqP9gCujj0mk"
 
 from langchain.chains import ConversationChain
 from langchain.memory import ConversationBufferMemory
